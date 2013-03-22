18031ab4a09157b31ce8492a051293022184fc7b
Kirill Klenov <horneds@gmail.com>
2013-03-22 19:39:40 +0800

    Proxy request to rpc

--- tests/rpc/dummy.py
+++ tests/rpc/dummy.py
@@ -1,3 +1,6 @@
+from adrest.resources.rpc import get_request
+
+
 def method1(name):
     return "Hello {0}".format(name)
 
@@ -6,5 +9,11 @@ def method2(start=1, end=100):
     from random import randint
     return randint(start, end)
 
+
 def error_method():
     raise Exception('Error here')
+
+
+@get_request
+def method3(request, name):
+    return request.method + name
