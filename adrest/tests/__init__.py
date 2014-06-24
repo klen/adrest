8a29b0992d3a904063c39ee782808dc537a0e7ee
Kirill Klenov <horneds@gmail.com>
2014-06-24 23:28:17 +0400

    Shortcut to request_resource.

--- adrest/tests/__init__.py
+++ adrest/tests/__init__.py
@@ -1,6 +1,6 @@
 """ Adrest test utils. """
 
-from .utils import AdrestClient, AdrestTestCase, reverse
+from .utils import AdrestClient, AdrestTestCase, reverse, request_resource
 from .client import AdrestRequestFactory
 
 # pylama:ignore=W0611
